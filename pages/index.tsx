a29076f2769499aa9f8e58b2b7bed712f7dab666
fares mohalhel <fares.mohalhel@gmail.com>
2023-04-25 02:29:28 +0300

    addex starting code project

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+
+/* 
+importing fonts???
+import { Inter } from 'next/font/google'
+const inter = Inter({ subsets: ['latin'] })
+*/ 
+
+
+export default function Home() {
+  return (
+    
+  )
+}
